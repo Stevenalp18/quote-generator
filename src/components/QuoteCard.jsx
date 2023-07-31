bad5a19a4676e927d3f5a6b780bd61d071a90a82
Steven Perez <stevenperez@stevens-air.myfiosgateway.com>
2023-07-31 18:45:15 -0400

    added new structure

---
+++ src/components/QuoteCard.jsx
@@ -0,0 +1,12 @@
+import { baseUrl } from "../data/baseUrl";
+
+const QuoteCard = () => {
+  return (
+    <div className="bg-red-500">
+      <div>Quote Here</div>
+      <div>Author</div>
+    </div>
+  );
+};
+
+export default QuoteCard;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import QuoteCard from "../components/QuoteCard";
+
+const MainPage = () => {
+  return (
+    <div
+      style="background-image: url('https://images.pexels.com/photos/547114/pexels-photo-547114.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')"
+      className="w-full h-full bg-center bg-no-repeat"
+    >
+      {/* <QuoteCard /> */}
+    </div>
+  );
+};
+
+export default MainPage;
