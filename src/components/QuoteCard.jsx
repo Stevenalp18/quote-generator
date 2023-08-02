2e22732fe9ec3a9dc3b7e03420569f94cb1e797f
Steven Perez <stevenalp18@gmail.com>
2023-08-01 22:20:27 -0400

    fixed random num, did not have randomized, added new icons and added a save quote option with no current function, will use with local storage

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -34,30 +34,39 @@ const QuoteCard = () => {
       return <div className="animate-bounce">Loading...</div>;
     } else {
       return (
-        <div>
-          <div className="mb-6">"{quotesData[randNum].quote}"</div>
+        <div className="flex flex-col border-2 p-6 sm:p-10 rounded-xl h-96">
+          <div className="mb-6 relative ">"{quotesData[randNum].quote}"</div>
           <div>- "{quotesData[randNum].author}"</div>
         </div>
       );
     }
   };
 
+  const handleClick = () => {
+    if (apiCheck()) {
+      console.log(randNum);
+      setRandNum(Math.floor(Math.random() * quotesData.length));
+    } else {
+      alert("api not yet loaded");
+    }
+  };
+
   return (
-    <div className="w-3/4 lg:w-1/3 rounded-xl m-auto relative top-1/4 p-4 sm:p-10 text-2xl sm:text-4xl border-2 font-['Amatic_SC']">
+    <div className="w-4/5 lg:w-2/5 text-[1.5rem] gap-10 m-auto relative top-20 sm:p-10 md:text-3xl lg:text-4xl font-['Amatic_SC']">
       {renderItem()}
-      <div>
+      <div className="text-2xl sm:text-3xl m-auto p-4 sm:px-6 flex justify-between">
         <button
-          className="mt-10"
+          className="hover:text-white active:text-slate-200"
           onClick={() => {
-            if (apiCheck()) {
-              console.log(randNum);
-              setRandNum(randNum + 1);
-            } else {
-              alert("api not yet loaded");
-            }
+            handleClick();
           }}
         >
+          <i className="fa-solid fa-arrows-rotate fa-sm" />
           Click for Quote
+        </button>
+        <button className="hover:text-white active:text-slate-200">
+          <i className="fa-solid fa-bookmark fa-sm" />
+          Save Quote
         </button>
       </div>
     </div>
